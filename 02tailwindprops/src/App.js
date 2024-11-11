61f1dc226c20aa8ca63f2adb65e4c0326c8dcdfd
madhura <madhuramadev@gmail.com>
2024-11-10 18:04:36 -0700

    learning projects

--- 02tailwindprops/src/App.js
+++ 02tailwindprops/src/App.js
@@ -1,12 +1,25 @@
 // import { useState } from 'react'
 import './App.css'
+import Card from './components/card';
 
 function App() {
   // const [count, setCount ] = useState(0)
 
+  let myObj = {
+    name: 'madhura',
+    age: 25,
+    address: {
+      city: 'mandya',
+      state: 'Karnataka',
+      country: 'India'
+    }
+  }
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   return (
     <>
     <h1 className='text-3xl bg-green-500 p-3'> CRA with Tailwind </h1>
+    <Card username="hitesh" myArr={newArr} />
+    <Card username='Json' post='Staff Engg.' />
     </>
   );
 }
